5cfb6254428e7c888beccd5fe04b6c33f9177d07
Gabriel Bernardes de Moura <gabrielbemoura@gmail.com>
2022-06-27 17:31:05 -0300

    Adicionado os metodos PUT e DELETE

--- src/main/java/com/gabriel/todolist/controller/TaskController.java
+++ src/main/java/com/gabriel/todolist/controller/TaskController.java
@@ -33,5 +33,17 @@ public class TaskController {
     public ResponseEntity<Task> getTaskById(@PathVariable(value = "id")Long id){
         return taskService.findTaskById(id);
     }
+
+    @PutMapping("/tasks/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Task> getTaskById(@PathVariable(value = "id")Long id, @RequestBody Task task){
+        return taskService.updateTaskById(task, id);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object> deleteById(@PathVariable(value = "id")Long id){
+        return taskService.deleteById(id);
+    }
 }
 
